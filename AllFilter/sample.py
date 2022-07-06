984a21d3aef21127a845ef7e912ae57515956c2a
UnluckyBoy <qfmy_zhang@163.com>
2022-07-06 17:48:44 +0800

    Changes--By Matrix

---
+++ AllFilter/sample.py
@@ -0,0 +1,103 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import os
+from torchvision import transforms
+from Vocabulary import Vocabulary
+from model import EncoderCNN, DecoderRNN
+from PIL import Image
+import cv2
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def load_image(image_path, transform=None):
+    image = Image.open(image_path).convert('RGB')
+    image = image.resize([224, 224], Image.LANCZOS)
+
+    if transform is not None:
+        image = transform(image).unsqueeze(0)
+
+    return image
+
+
+def main(args):
+    # Image preprocessing
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
+
+    # Load vocabulary wrapper
+    with open(args.vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
+
+    # Build models
+    encoder = EncoderCNN(args.embed_size).eval()  # eval mode (batchnorm uses moving mean/variance)
+    decoder = DecoderRNN(args.embed_size, args.hidden_size, len(vocab), args.num_layers)
+    encoder = encoder.to(device)
+    decoder = decoder.to(device)
+
+    # Load the trained model parameters
+    encoder.load_state_dict(torch.load(args.encoder_path))
+    decoder.load_state_dict(torch.load(args.decoder_path))
+
+    # Prepare an image
+    image = load_image(args.image, transform)
+    image_tensor = image.to(device)
+
+    # Generate an caption from the image
+    feature = encoder(image_tensor)
+    sampled_ids = decoder.sample(feature)
+    sampled_ids = sampled_ids[0].cpu().numpy()  # (1, max_seq_length) -> (max_seq_length)
+
+    # Convert word_ids to words
+    sampled_caption = []
+    for word_id in sampled_ids:
+        word = vocab.idx2word[word_id]
+        sampled_caption.append(word)
+        if word == '<end>':
+            break
+    sentence = ' '.join(sampled_caption)
+
+    # Print out the image and the generated caption
+    #打印出最终识别结果并绘制
+    print(args.image+"_最终识别结果："+sentence)
+
+    #以plt.imshow(np.asarray(mImage))绘制
+    image = Image.open(args.image)
+    # 解决中文显示问题
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+
+    plt.title("原图生物")#title
+    plt.imshow(np.asarray(image))
+
+    ###以openCV绘制
+    #img1 = cv2.imread(args.image, flags=1)  # flags=1读取彩色图像(BGR)
+    #imageRGB = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)  # 图片格式转换：BGR(OpenCV) -> RGB(PyQt5)
+    #plt.imshow(imageRGB)  # matplotlib 显示彩色图像(RGB格式)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
+    #parser.add_argument('--encoder_path', type=str, default='models/encoder-5-3000.pkl',help='path for trained encoder')
+    parser.add_argument('--encoder_path', type=str, default='models/encoder-5-3000.ckpt',
+                        help='path for trained encoder')
+    #parser.add_argument('--decoder_path', type=str, default='models/decoder-5-3000.pkl',help='path for trained decoder')
+    parser.add_argument('--decoder_path', type=str, default='models/decoder-5-3000.ckpt',
+                        help='path for trained decoder')
+    parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
+
+    # Model parameters (should be same as paramters in train.py)
+    parser.add_argument('--embed_size', type=int, default=256, help='dimension of word embedding vectors')
+    parser.add_argument('--hidden_size', type=int, default=512, help='dimension of lstm hidden states')
+    parser.add_argument('--num_layers', type=int, default=1, help='number of layers in lstm')
+    args = parser.parse_args()
+    main(args)
--- Scripts/nlp.py
+++ Scripts/nlp.py
@@ -9,6 +9,10 @@ import time
 x=torch.rand([50,1])
 y=3*x+0.8
 
+# 定义训练环境  torch.cuda.is_available()判断是否支持cuda,即gpu训练;否则以cpu
+mDevice = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+x, y = x.to(mDevice), y.to(mDevice)  # 将x、y进行类型转化
+
 #定义模型
 class NLP_Model(nn.Module):
     def __init__(self):
@@ -20,38 +24,42 @@ class NLP_Model(nn.Module):
         out=self.linear(x)
         return out
 
-#定义训练环境  torch.cuda.is_available()判断是否支持cuda,即gpu训练;否则以cpu
-mDevice=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-x,y=x.to(mDevice),y.to(mDevice)#将x、y进行类型转化
+def main():
+    # 定义训练环境  torch.cuda.is_available()判断是否支持cuda,即gpu训练;否则以cpu
+    #mDevice = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #x, y = x.to(mDevice), y.to(mDevice)  # 将x、y进行类型转化
 
-#实例化模型、优化器类实例化、loss实例化
-mNLP_Model=NLP_Model().to(mDevice)#将模型进行类型转化
-mOptim=SGD(mNLP_Model.parameters(),0.001)
-mLoss=nn.MSELoss()
+    # 实例化模型、优化器类实例化、loss实例化
+    mNLP_Model = NLP_Model().to(mDevice)  # 将模型进行类型转化
+    mOptim = SGD(mNLP_Model.parameters(), 0.001)
+    mLoss = nn.MSELoss()
 
-#循环，进行梯度下降，参数更新
-for i in range(40000):
-    #获取预测值
-    y_predict=mNLP_Model(x)
-    loss=mLoss(y_predict,y)
-    #梯度预置为0
-    mOptim.zero_grad()
-    #反向传播
-    loss.backward()
-    #设置参数更新
-    mOptim.step()
-    if i%20==0:
-        #params=list(mNLP_Model.parameters())
-        #print(loss.item(),params[0].item(),params[1].item())
-        print('Epoch[{}/{}],loss:{:.6f}'.format(i,40000,loss.data))
+    # 循环，进行梯度下降，参数更新
+    for i in range(40000):
+        # 获取预测值
+        y_predict = mNLP_Model(x)
+        loss = mLoss(y_predict, y)
+        # 梯度预置为0
+        mOptim.zero_grad()
+        # 反向传播
+        loss.backward()
+        # 设置参数更新
+        mOptim.step()
+        if i % 20 == 0:
+            # params=list(mNLP_Model.parameters())
+            # print(loss.item(),params[0].item(),params[1].item())
+            print('Epoch[{}/{}],loss:{:.6f}'.format(i, 40000, loss.data))
 
-#模型评估
-mNLP_Model.eval()#设置模型为评估模型
-predict=mNLP_Model(x)
-#predict=predict.data.numpy() #CPU环境
-#plt.scatter(x.data.numpy(),y.data.numpy(),c="r")
-#plt.plot(x.data.numpy(),predict)
-predict=predict.cpu().detach().numpy() #GPU环境,detach()与data类似；detach()是深拷贝，data为浅拷贝
-plt.scatter(x.cpu().detach().numpy(),y.cpu().detach().numpy(),c="r")
-plt.plot(x.cpu().detach().numpy(),predict)
-plt.show()
+    # 模型评估
+    mNLP_Model.eval()  # 设置模型为评估模型
+    predict = mNLP_Model(x)
+    # predict=predict.data.numpy() #CPU环境
+    # plt.scatter(x.data.numpy(),y.data.numpy(),c="r")
+    # plt.plot(x.data.numpy(),predict)
+    predict = predict.cpu().detach().numpy()  # GPU环境,detach()与data类似；detach()是深拷贝，data为浅拷贝
+    plt.scatter(x.cpu().detach().numpy(), y.cpu().detach().numpy(), c="r")
+    plt.plot(x.cpu().detach().numpy(), predict)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
