c3390bf5f6ac03d881e5b2c1b4351dee2dead422
UnluckyBoy <qfmy_zhang@163.com>
2022-07-07 15:12:55 +0800

    Changes--By Matrix

---
+++ AllFilter/build_vocab.py
@@ -0,0 +1,58 @@
+import nltk
+import pickle
+import argparse
+from collections import Counter
+from pycocotools.coco import COCO
+from Vocabulary import Vocabulary
+
+
+"""此处Vocabulary无法被调用，另写"""
+
+def build_vocab(json, threshold):
+    """创建简单的词汇表包装器"""
+    coco = COCO(json)
+    counter = Counter()
+    ids = coco.anns.keys()
+    for i, id in enumerate(ids):
+        caption = str(coco.anns[id]['caption'])
+        tokens = nltk.tokenize.word_tokenize(caption.lower())
+        counter.update(tokens)
+
+        if (i+1) % 1000 == 0:
+            print("[{}/{}] Tokenized the captions.".format(i+1, len(ids)))
+
+    #检测频率大于等于threshold的单词并放入词汇表
+    words = [word for word, cnt in counter.items() if cnt >= threshold]
+
+    # 创建一个词汇表包装器并添加特殊的令牌
+    vocab = Vocabulary()
+    vocab.add_word('<pad>')
+    vocab.add_word('<start>')
+    vocab.add_word('<end>')
+    vocab.add_word('<unk>')
+
+    # Add the words to the vocabulary.
+    for i, word in enumerate(words):
+        vocab.add_word(word)
+    return vocab
+
+def main(args):
+    vocab = build_vocab(json=args.caption_path, threshold=args.threshold)
+    vocab_path = args.vocab_path
+    with open(vocab_path, 'wb') as f:
+        pickle.dump(vocab, f)
+    print("总词汇量: {}".format(len(vocab)))
+    print("词汇表保存地址:'{}'".format(vocab_path))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--caption_path', type=str,
+                        default='coco_data/annotations/captions_train2014.json',
+                        help='注释文件地址')
+    parser.add_argument('--vocab_path', type=str, default='./coco_data/vocab.pkl',
+                        help='词汇表文件地址')
+    parser.add_argument('--threshold', type=int, default=4,
+                        help='词汇阈值')
+    args = parser.parse_args()
+    main(args)
